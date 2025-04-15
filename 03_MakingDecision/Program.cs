25850313a60bb3a88c540893373c5c07d1e2a925
Ömer Üçüncü <118854548+omerrucuncu@users.noreply.github.com>
2025-04-15 18:20:02 +0300

    Ders 3

---
+++ 03_MakingDecision/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_MakingDecision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region If Else
+
+            //Console.Write("Lütfen şifreyi giriniz: ");
+            //string password;
+            //password = Console.ReadLine();
+            //if (password == "abcd")
+            //{
+            //    Console.WriteLine("Şifre Doğru");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Şifre Yanlış");
+            //}
+
+            //string capital, country;
+
+            //Console.Write("Lütfen başkenti giriniz : ");
+            //capital = Console.ReadLine();
+
+            //Console.Write("Lütfen ülkeyi giriniz : ");
+            //country = Console.ReadLine();
+
+            //if (capital == "ankara" & country == "türkiye")
+            //{
+            //    Console.WriteLine("Doğru bilgi");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Hatalı Bilgi");
+            //}
+
+            //int number;
+
+            //Console.Write("Sayıyı giriniz: ");
+            //number = int.Parse(Console.ReadLine());
+
+            //if (number == 5)
+            //{
+            //    Console.WriteLine("Doğru sayı");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Yanlış sayı");
+            //}
+
+            //int exam1, exam2, exam3;
+            //double average;
+            //string result = "";
+
+            //Console.Write("1.Sınavı giriniz: ");
+            //exam1 = int.Parse(Console.ReadLine());
+
+            //Console.Write("2.Sınavı giriniz: ");
+            //exam2 = int.Parse(Console.ReadLine());
+
+            //Console.Write("3.Sınavı giriniz: ");
+            //exam3 = int.Parse(Console.ReadLine());
+
+            //average = (exam1 + exam2 + exam3) / 3;
+            //Console.WriteLine("Ortalamanız : " + average);
+
+            //if (average >= 0 & average < 50)
+            //{
+            //    result = "Sonuç vasat";
+            //}
+            //else if (average >= 50 & average <= 70)
+            //{
+            //    result = "Orta";
+            //}
+            //else if (average > 70 & average <= 84)
+            //{
+            //    result = "İyi";
+            //}
+            //else if (average > 84 & average <= 100)
+            //{
+            //    result = "Çok iyi";
+            //}
+            //else
+            //{
+            //    result = "Hata!";
+            //}
+
+            //Console.WriteLine(result);
+
+
+
+
+
+            Console.Read();
+
+            #endregion
+        }
+    }
+}
