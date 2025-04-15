4acd53dd093253bb44b6b7019e95ad98ab3b0c76
Ömer Üçüncü <118854548+omerrucuncu@users.noreply.github.com>
2025-04-15 16:52:09 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region YazdırmaKomutları
+
+            //Console.WriteLine("Merhaba Dünya");
+            //Console.Write("Selam");
+
+            Console.WriteLine("***** Yemek Kategorileri *****");
+            Console.WriteLine();
+            Console.WriteLine("1-Çorbalar");
+            Console.WriteLine("2-Ana Yemekler");
+            Console.WriteLine("3-Soğuk Başlangıçlar");
+            Console.WriteLine("4-Salatalar");
+            Console.WriteLine("5-Tatlılar");
+            Console.WriteLine("6-İçecekler");
+            Console.WriteLine();
+            Console.WriteLine("***** Yemek Kategorileri ******");
+
+            #endregion
+
+            #region Değişkenler
+
+
+
+            #endregion
+
+            Console.Read();
+
+            
+        }
+    }
+}
+
+
+
+
+
+
+
+// Yazdırma Komutları
---
+++ 02_Variables/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Double Değişkenler
+
+            //double number;
+
+            //number = 4.33;
+
+            //Console.WriteLine(number);
+
+            //Console.WriteLine("***** Fiyat Listesi *****");
+            //Console.WriteLine();
+
+            //double applePrice, orangePrice, strawberryPrice, potatoPrice, tomatoPrice;
+
+            //applePrice = 14.74;
+            //orangePrice = 20.22;
+            //strawberryPrice = 45;
+            //potatoPrice = 4.332;
+            //tomatoPrice = 5.44;
+
+            //Console.WriteLine("---- Elma birim fiyatı :" + applePrice + " TL"); 
+            //Console.WriteLine("---- Portakal birim fiyatı :" + orangePrice + " TL"); 
+            //Console.WriteLine("---- Çilek birim fiyatı: " + strawberryPrice + " TL"); 
+            //Console.WriteLine("---- Patates birim fiyatı: " + potatoPrice + " TL"); 
+            //Console.WriteLine("---- Domates birim fiyatı: " + tomatoPrice + " TL");
+
+
+            #endregion
+
+            #region Klavyeden veri girişleri String değişkenler
+
+            //Console.WriteLine("***** CSharp Hava Yolları *****");
+
+            //string passengerName, passengerSurname, passengerGender, passengerDistrict, passengerCity, passengerIdentifyNumber, passengerAge;
+
+            //Console.Write("Yolcu Adı : ");
+            //passengerName = Console.ReadLine();
+
+                
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
